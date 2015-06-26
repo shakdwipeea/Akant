d49c08629a0fa11ad718bf90af33e87287a5275b
shakdwipeea <ashakdwipeea@gmail.com>
2015-06-27 02:30:48 +0530

    Added barebones confirmation form.

---
+++ Akant/Home.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework.Controls;
+namespace Akant
+{
+    public partial class Home : MetroForm
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            metroProgressBar1.Visible = true;
+            this.Hide();
+            var postForm = new PostActivate();
+            postForm.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ Akant/PostActivate.cs
@@ -0,0 +1,24 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Akant
+{
+    public partial class PostActivate : MetroForm
+    {
+        public PostActivate()
+        {
+            InitializeComponent();
+        }
+
+        private void PostActivate_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
